6ac56652c9b4da348caee7f60a9e90b560c27ab0
Aida <45520372+iEEEda@users.noreply.github.com>
2019-06-20 02:16:59 +0600

    Add files via upload

---
+++ Colorizer/main/java/kz/eduard/colorizer/MainActivity.java
@@ -0,0 +1,123 @@
+package kz.eduard.colorizer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private int[] imageResIds = {R.drawable.cuba1, R.drawable.cuba2, R.drawable.cuba3};
+    private int imageIndex = 0;
+    private boolean color = true;
+    private boolean red = true;
+    private boolean green = true;
+    private boolean blue = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        loadImage();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        Drawable nextImageDrawable = menu.findItem(R.id.nextImage).getIcon();
+        nextImageDrawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+
+        menu.findItem(R.id.red).setChecked(red);
+        menu.findItem(R.id.green).setChecked(green);
+        menu.findItem(R.id.blue).setChecked(blue);
+
+        menu.setGroupVisible(R.id.colorGroup, color);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nextImage:
+                imageIndex++;
+                if(imageIndex >= imageResIds.length){
+                    imageIndex = 0;
+                }
+                loadImage();
+                break;
+            case R.id.color:
+                color = !color;
+                updateSaturation();
+                invalidateOptionsMenu();
+                break;
+            case R.id.red:
+                red = !red;
+                updateColors();
+                item.setChecked(red);
+                break;
+            case R.id.green:
+                green = !green;
+                updateColors();
+                item.setChecked(green);
+                break;
+            case R.id.blue:
+                blue = !blue;
+                updateColors();
+                item.setChecked(blue);
+                break;
+            case R.id.reset:
+                imageView.clearColorFilter();
+                red = green = blue = color = true;
+                invalidateOptionsMenu();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadImage() {
+        Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+    }
+
+    private void updateSaturation(){
+        ColorMatrix colorMatrix = new ColorMatrix();
+        if(color){
+            red = green = blue = true;
+            colorMatrix.setSaturation(1);
+        } else {
+            colorMatrix.setSaturation(0);
+        }
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+
+    private void updateColors() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        float[] matrix = {
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0,
+        };
+        if(!red) matrix[0] = 0;
+        if(!green) matrix[6] = 0;
+        if(!blue) matrix[12] = 0;
+        colorMatrix.set(matrix);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+}
