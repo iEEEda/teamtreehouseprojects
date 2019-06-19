966f121380f1a7918cc2682a91859be43dbf30f4
Aida <45520372+iEEEda@users.noreply.github.com>
2019-06-20 00:27:55 +0600

    Add files via upload

---
+++ InteractiveStory/main/java/kz/eduard/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,107 @@
+package kz.eduard.interactivestory.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Stack;
+
+import kz.eduard.interactivestory.R;
+import kz.eduard.interactivestory.model.Page;
+import kz.eduard.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private String name;
+    private Stack<Integer> pageStack = new Stack<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+
+        name = intent.getStringExtra(getString(R.string.key_name));
+        if(name == null || name.isEmpty()){
+            name = "Friend";
+        }
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
+
+        final Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        //Add a name if placeholder included. Won't add if not
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+        if(page.isFinalPage()){
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loadPage(0);
+                    //finish();
+                }
+            });
+        } else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if(pageStack.isEmpty()) {
+            super.onBackPressed();
+        } else {
+            loadPage(pageStack.pop());
+        }
+    }
+}
